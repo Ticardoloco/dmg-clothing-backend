685f30ac4f399ab9278fa6b428ad6e4408074bf5
ticardoloco <egbeleketaiwo@gmail.com>
2026-04-10 15:27:35 +0100

    feat: add: (app, database, index, constant,) to connect to mongoDB

---
+++ backend/src/config/database.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () =>{
+   try {
+     const instanceConnect = await mongoose.connect(`${process.env.MONGODB_URI}`);
+    console.log(`\n mongoDb connected!!!
+        ${instanceConnect.connection.host}`);
+   } catch (error) {
+    console.log("mongoDb connection failed!", error);
+   }
+    
+}
+
+export default connectDB;
